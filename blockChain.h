f7fe79e86602ccdece4448f9dd454d304911189a
Ruben Gonzalez <98985433+RBNGNZLZ@users.noreply.github.com>
2022-11-27 22:23:10 -0600

    Update blockChain.h
    added comments

--- blockChain.h
+++ blockChain.h
@@ -7,13 +7,13 @@ class blockChain
 {
 private:
   std::vector<block> chain;
-  block createGenesisBlock() { return block(0, "Genesis"); }
+  block createGenesisBlock() { return block(0, "Genesis"); } //creates first block in blockchain
   
 public:
-  blockChain() { chain.push_back(createGenesisBlock()); }
-  void addBlock(int prevHash, std::string data) { chain.push_back(block(prevHash, data)); }
-  block getLatestBlock() { return chain[chain.size() - 1]; }
-  void printStats() { for(int i = 0; i < chain.size(); i++) {std::cout << chain[i].getHash() << ": '" << chain[i].getData() << "'" << std::endl; }}
+  blockChain() { chain.push_back(createGenesisBlock()); } //stores genesis block in blockchain
+  void addBlock(int prevHash, std::string data) { chain.push_back(block(prevHash, data)); } //adds next block 
+  block getLatestBlock() { return chain[chain.size() - 1]; } //returns recent block in blockchain
+  void printStats() { for(int i = 0; i < chain.size(); i++) {std::cout << chain[i].getHash() << ": '" << chain[i].getData() << "'" << std::endl; }} //returns block data and hash information
 
 };
 
