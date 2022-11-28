232b0cdbdb7ee6de2eda75019a3d6ede0f0ab98d
Ruben Gonzalez <98985433+RBNGNZLZ@users.noreply.github.com>
2022-11-27 22:18:31 -0600

    Update block.cpp
    added comments

--- block.cpp
+++ block.cpp
@@ -1,11 +1,11 @@
 #include "block.h"
-unsigned long block::calculateHash()
+unsigned long block::calculateHash() //calculates hash using data from current block 
   {
     std::string temp = data;
     return std::hash<std::string>()(temp.append(std::to_string(pHash)));
   }
 
-  block::block(int prevHash, std::string newData)
+  block::block(int prevHash, std::string newData) //updates block variables
   {
     data = newData;
     pHash = prevHash;
